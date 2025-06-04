d8b015a646fd47e2598aaa301fdf6843a5b95e8c
sterindev <sterinjacob2003@gmail.com>
2025-06-04 21:52:30 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -10,7 +10,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order for frontend 
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "https://food-del453.netlify.app/"
+    const frontend_url = "https://food-del453.netlify.app"
 
 
 
